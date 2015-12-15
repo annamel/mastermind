a0a17ab1357c0d7c52c2d6290a1eb2f84b302561
Vladimir Nikulichev <saturnus@yandex-team.ru>
2015-12-15 22:20:03 +0300

    Static loggers and config for tests

---
+++ tests/collector/util/app.cpp
@@ -0,0 +1,45 @@
+/*
+   Copyright (c) YANDEX LLC, 2015. All rights reserved.
+   This file is part of Mastermind.
+
+   Mastermind is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 3.0 of the License, or (at your option) any later version.
+
+   Mastermind is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with Mastermind.
+*/
+
+#include <WorkerApplication.h>
+
+namespace app {
+
+ioremap::elliptics::logger_base & logger()
+{
+    static ioremap::elliptics::file_logger logger("/dev/stdout", DNET_LOG_DEBUG);
+    return logger;
+}
+
+ioremap::elliptics::logger_base & elliptics_logger()
+{
+    return logger();
+}
+
+Config & test_config()
+{
+    static Config config;
+    return config;
+}
+
+const Config & config()
+{
+    return test_config();
+}
+
+} // namespace app
