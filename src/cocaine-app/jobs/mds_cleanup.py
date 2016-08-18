d02842bffac9b61991b6a388742f98e009b1ae1c
aniam <aniam@yandex-team.ru>
2016-08-18 17:21:46 +0300

    Introducing langolier minion worker
    Cleanup: global renaming
    
    TYPE_CLEANUP -> TYPE_TTL_CLEANUP
    mds_cleanup -> ttl_cleanup
    langolier -> cleanup
    
    There is still reference to mds, since the cmd itself is named mds_cleanup

--- src/cocaine-app/jobs/mds_cleanup.py
+++ src/cocaine-app/jobs/mds_cleanup.py
@@ -3,6 +3,7 @@ from job import Job
 from job_types import JobTypes
 import logging
 import storage
+from infrastructure import infrastructure
 
 logger = logging.getLogger('mm.jobs')
 
@@ -16,13 +17,13 @@ class MdsCleanupJob(Job):
         'attemps',
         'nproc',
         'wait_timeout',
-        'safe'
+        'safe',
+        'resources'
     )
 
     def __init__(self, **kwargs):
         super(MdsCleanupJob, self).__init__(**kwargs)
-        self.type = JobTypes.TYPE_CLEANUP
-        logger.error('MDS cleanup job inited')
+        self.type = JobTypes.TYPE_TTL_CLEANUP
 
     def _set_resources(self):
         self.resources = {
@@ -43,11 +44,29 @@ class MdsCleanupJob(Job):
         self.resources[Job.RESOURCE_FS].append((nb.node.host.addr, str(nb.fs.fsid)))
 
     def create_tasks(self):
-        logger.error("Cleanup job: groups={} (iter={}), batch_size={}, remotes={}, attemps={}".format(
-            self.groups, self.iter_group, self.batch_size, self.remotes, self.attemps))
-        self.tasks = {}
-        # trace_id = self.id[:16]
-        # XXX: tmp_dir, log_path, log_level
+
+        # Log, Log_level, temp to be taken from config on infrastructure side
+        langolier_cmd = infrastructure.cleanup_cmd(
+            remotes=self.remotes,
+            groups=self.groups,
+            iter_group=self.iter_group,
+            wait_timeout=self.wait_timeout,
+            batch_size=self.batch_size,
+            attemps=self.attemps,
+            nproc=self.nproc,
+            trace_id=int(self.id[:16], 16))
+
+        if self.iter_group not in storage.groups:
+            logger.error("Not valid iter group is specified for create task %s", self.iter_group)
+            return None
+
+        logger.debug("Cleanup job: Set for execution task %s", langolier_cmd)
+
+        # Run langolier on the storage node where we are going to iterate
+        nb = storage.groups[self.iter_group].node_backends[0]
+        host = nb.node.host.addr
+        task = tasks.MinionCmdTask.new(self, host=host, group=self.iter_group, cmd=langolier_cmd, params={})
+        self.tasks.append(task)
 
     @property
     def _involved_groups(self):
