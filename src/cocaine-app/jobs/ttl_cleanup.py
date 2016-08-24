9f24c0d3d1f1679bccb1bfc9a80bbd56f9596940
aniam <aniam@yandex-team.ru>
2016-08-24 13:03:14 +0300

    Fix arguments for cleanup run
    Fix defaults.
    If not specified, they would be taken from the corresponding section in config
    
    Adding dry-run option for cleanup job
    
    STYLE fixes: TYPE_TTL_CLEANUP -> TYPE_TTL_CLEANUP_JOB
    
    STYLE no langolier in comments on TTL cleanup
    
    STYLE dry-run is more popular in MM then safe. So convert mm flag named dry-run into safe only when going to mds_command directly
    
    STYLE cleanup_cmd formatting is improved:
    
    long names of args instead of short ones
    all args to be present in formal specification, not added afterwards
    cleanup_cmd -> ttl_cleanup_cmd
    None arguments to be successfully extracted from the config if there would be something
    or instead of trenary operator
    
    STYLE fixes for Job:
    
    Renaming MdsCleanupJob to TtlCleanupJob
    Renaming mds_cleanup.py to ttl_cleanup.py
    Fix formatting
    Fix log messages
    
    Fix resource consumption description for Ttl Cleanup job
    
    Andrey's comment: outgoing remove cmd do not consume lots, while incoming iteration data generate large traffic

--- src/cocaine-app/jobs/ttl_cleanup.py
+++ src/cocaine-app/jobs/ttl_cleanup.py
@@ -7,7 +7,8 @@ from infrastructure import infrastructure
 
 logger = logging.getLogger('mm.jobs')
 
-class MdsCleanupJob(Job):
+
+class TtlCleanupJob(Job):
 
     PARAMS = (
         'groups',
@@ -17,55 +18,62 @@ class MdsCleanupJob(Job):
         'attemps',
         'nproc',
         'wait_timeout',
-        'safe',
+        'dry_run',
         'resources'
     )
 
     def __init__(self, **kwargs):
-        super(MdsCleanupJob, self).__init__(**kwargs)
-        self.type = JobTypes.TYPE_TTL_CLEANUP
+        super(TtlCleanupJob, self).__init__(**kwargs)
+        self.type = JobTypes.TYPE_TTL_CLEANUP_JOB
 
     def _set_resources(self):
         self.resources = {
             Job.RESOURCE_HOST_IN: [],
             Job.RESOURCE_HOST_OUT: [],
             Job.RESOURCE_FS: [],
-            }
+        }
 
         if self.iter_group not in storage.groups:
-            logger.error("Not valid iter group is specified %s", self.iter_group)
+            logger.error("Specified iter group {} has not been found".format(self.iter_group))
             return None
 
         # The nodes that are not iterated would be asked to perform remove
         # operation. No data is written. And no data is read.
         # While iteration group node is working heavily
         nb = storage.groups[self.iter_group].node_backends[0]
-        self.resources[Job.RESOURCE_HOST_OUT].append(nb.node.host.addr)
+        self.resources[Job.RESOURCE_HOST_IN].append(nb.node.host.addr)
         self.resources[Job.RESOURCE_FS].append((nb.node.host.addr, str(nb.fs.fsid)))
 
     def create_tasks(self):
 
         # Log, Log_level, temp to be taken from config on infrastructure side
-        langolier_cmd = infrastructure.cleanup_cmd(
+        ttl_cleanup_cmd = infrastructure.ttl_cleanup_cmd(
             remotes=self.remotes,
             groups=self.groups,
             iter_group=self.iter_group,
             wait_timeout=self.wait_timeout,
             batch_size=self.batch_size,
-            attemps=self.attemps,
+            attempts=self.attemps,
             nproc=self.nproc,
-            trace_id=int(self.id[:16], 16))
+            trace_id=int(self.id[:16], 16),
+            safe=self.dry_run)
 
         if self.iter_group not in storage.groups:
-            logger.error("Not valid iter group is specified for create task %s", self.iter_group)
+            logger.error("Specified iter group {} has not been found".format(self.iter_group))
             return None
 
-        logger.debug("Cleanup job: Set for execution task %s", langolier_cmd)
+        logger.debug("TTl cleanup job: Set for execution task %s", ttl_cleanup_cmd)
 
         # Run langolier on the storage node where we are going to iterate
         nb = storage.groups[self.iter_group].node_backends[0]
         host = nb.node.host.addr
-        task = tasks.MinionCmdTask.new(self, host=host, group=self.iter_group, cmd=langolier_cmd, params={})
+        task = tasks.MinionCmdTask.new(
+            self,
+            host=host,
+            group=self.iter_group,
+            cmd=ttl_cleanup_cmd,
+            params={},
+        )
         self.tasks.append(task)
 
     @property
@@ -75,4 +83,3 @@ class MdsCleanupJob(Job):
     @property
     def _involved_couples(self):
         return []
-
